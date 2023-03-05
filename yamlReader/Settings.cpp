d989144cc334d8f9010518108bbba2304b8d42be
CiopeicaSergiu <sergiu-andrei.ciopeica@student.tuiasi.ro>
2023-03-05 14:29:41 +0200

    I am fast as fuck boy.

--- yamlReader/Settings.cpp
+++ yamlReader/Settings.cpp
@@ -2,13 +2,23 @@
 #include </home/sergiu/Desktop/CSD/Proiect/dependencies/yaml/include/yaml-cpp/yaml.h>
 #include <iostream>
 
+template <typename T> void printArray(const std::vector<T> &arr) {
+  std::cout << "\nArr: ";
+  for (const auto &el : arr) {
+    std::cout << el << ", ";
+  }
+  std::cout << "\n";
+}
+
 void Settings::loadYamlFile(const std::string &filePath) {
 
   YAML::Node serverSettings = YAML::LoadFile(filePath);
 
   serverIp = serverSettings["serverIp"].as<std::string>();
   std::cout << serverIp << "\n";
-  // serverSettings["nodesIp"].as<std::vector<std::string>>();
+  nodesIp = serverSettings["nodesIp"].as<std::vector<std::string>>();
+
+  printArray(nodesIp);
 }
 
 Settings::Settings(const std::string filePath) { loadYamlFile(filePath); }
